87ade3ebaafb462b9b6ded1d7afb2bcddda3dc4f
Conner Krenzer <krenzer84@gmail.com>
2025-01-09 12:38:05 -0500

    'GP finished'

--- api/fruits/fruits-model.js
+++ api/fruits/fruits-model.js
@@ -1,23 +1,23 @@
-const knex = require('knex')
+const knex = require("knex");
 
 const db = knex({
-  client: 'sqlite3',
+  client: "sqlite3",
   connection: {
-    filename: './data/produce.db3'
+    filename: "./data/produce.db3",
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
 });
 
 function getAll() {
-  return db('fruits');
+  return db("fruits");
 }
 
 function getById(id) {
-  return db('fruits').where('id', id).first();
+  return db("fruits").where("fruit_id", id).first();
 }
 
 async function create(fruit) {
-  const [id] = await db('fruits').insert(fruit);
+  const [id] = await db("fruits").insert(fruit);
   return getById(id);
 }
 
---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/produce.db3",
+    },
+    migrations: {
+      directoy: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {},
+
+  production: {},
+
+  testing: {},
+};
